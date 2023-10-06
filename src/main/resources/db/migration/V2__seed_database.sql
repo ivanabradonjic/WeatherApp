eeb238b9307fb6d9d22cc3d2ccb7b1336abbac4a
ivanabradonjic <ivana.bradonjic@gmail.com>
2023-10-06 14:48:06 +0200

    create table city and insert data

---
+++ src/main/resources/db/migration/V2__seed_database.sql
@@ -0,0 +1,3 @@
+INSERT INTO city (name, country_code) VALUES ('Novi Sad', 'sr');
+INSERT INTO city (name, country_code) VALUES ('Belgrade', 'sr');
+INSERT INTO city (name, country_code) VALUES ('London', 'uk');

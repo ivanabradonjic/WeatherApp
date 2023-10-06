5e35042e8ffcba97f13e528c1a2f9facf337f6da
ivanabradonjic <ivana.bradonjic@gmail.com>
2023-10-06 14:46:31 +0200

    create model for city

---
+++ src/main/java/com/valcon/WeatherApp/model/City.java
@@ -0,0 +1,48 @@
+package com.valcon.WeatherApp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String countryCode;
+
+    public City() {
+    }
+
+    public City(String name, String countryCode) {
+        this.name = name;
+        this.countryCode = countryCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+}

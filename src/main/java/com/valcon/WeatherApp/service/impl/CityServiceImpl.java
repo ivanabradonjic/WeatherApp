47b0489357593dcf632d29155fe6fca8eb0e7010
ivanabradonjic <ivana.bradonjic@gmail.com>
2023-10-08 09:41:15 +0200

    [VAL-3] change files need to implement method for calculating average temperature

--- src/main/java/com/valcon/WeatherApp/service/impl/CityServiceImpl.java
+++ src/main/java/com/valcon/WeatherApp/service/impl/CityServiceImpl.java
@@ -5,6 +5,7 @@ import com.valcon.WeatherApp.mapper.CityMapper;
 import com.valcon.WeatherApp.model.City;
 import com.valcon.WeatherApp.repository.CityRepository;
 import com.valcon.WeatherApp.service.CityService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,11 @@ public class CityServiceImpl implements CityService {
         this.cityRepository = cityRepository;
     }
 
+    @Override
+    public City getByName(String name) {
+        return cityRepository.findByName(name).orElseThrow(() -> new EntityNotFoundException("City with name"+name+"hasn't been found"));
+    }
+
     @Transactional(readOnly = true)
     @Override
     public List<CityResponseDTO> getAll() {
@@ -28,5 +34,10 @@ public class CityServiceImpl implements CityService {
     }
     public List<City> getAllCities(){
         return cityRepository.findAll();
+    }
+
+    @Override
+    public City getById(Long id) {
+        return cityRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("City with id = "+ id+"hasn't been found"));
     }
 }
